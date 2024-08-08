785876bfcfdde1691f5d6f8ff9b825efb794b3bd
hilkkie <hilkka.hannikainen@outlook.com>
2024-08-08 13:56:48 +0300

    Implemented model using GRU unit. Implemented training for three different models and visualized the results.

--- stock_price_rnn.py
+++ stock_price_rnn.py
@@ -265,3 +265,175 @@ ax.xaxis.set_major_locator(mdates.DayLocator(interval=7))
 ax.xaxis.set_minor_locator(mdates.DayLocator())
 
 plt.show()
+
+# %%
+# model using GRU
+class GRU(nn.Module):
+    def __init__(self, n_input=1, n_hidden=10, n_output=1):
+        super(GRU, self).__init__()
+        self.n_hidden = n_hidden
+        self.gru = nn.GRU(n_input, n_hidden)
+        self.linear = nn.Linear(n_hidden, n_output)
+        
+    def forward(self, x, h_0):
+        output, h_n = self.gru(x, h_0)
+        y = self.linear(output)
+        return y[-1], h_n
+    
+    def init_hidden(self, batch_size):
+        # initialize hidden states to zeros
+        return torch.zeros([1, batch_size, self.n_hidden], dtype=torch.float)
+    
+    def forecast(self, x, period_length, scaler=None):
+        # forecast a sequence of length period_length
+        y_fct = torch.zeros(period_length)
+        
+        self.eval()
+        with torch.no_grad():
+            for n in range(period_length):
+                hidden = self.init_hidden(1)
+                y, _ = self.forward(x.reshape((len(x), 1, 1)), hidden)
+        
+                # update the state
+                x = torch.cat((x, y.flatten()))[1:]
+                y_fct[n] = y.flatten()
+                
+        # scale the output if transformation is given
+        if scaler is not None:
+            y_fct = torch.from_numpy(scaler.inverse_transform(y_fct.numpy().reshape(-1, 1)))
+            
+        return y_fct
+    
+    def calculate_loss(self, test_data, scale_output=False):
+        # calculate loss over test data
+        x, targets = test_data.inputs, test_data.targets
+        scaler = test_data.scaler if scale_output else None
+        y = self.forecast(x, len(targets), scaler=scaler)
+                
+        return F.mse_loss(y, targets)
+    
+
+    def train_model(self, N_epochs, learning_rate, train_dl, test_data,
+                    lr_scheduler_func=None, lr_params=None):
+        # initialize optimizer
+        optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        # initialize learning rate scheduler, if given
+        if lr_scheduler_func is not None:
+            lr_scheduler = lr_scheduler_func(optimizer, *lr_params)
+        else:
+            lr_scheduler = None
+
+        start_time = time.time()
+        for n in range(N_epochs):
+    
+            losses = []
+            test_losses = []
+    
+            for inputs, outputs in train_dl:
+                btch_len, seq_len = inputs.shape
+                # format the input for LSTM: batch size is the second dimension
+                x = inputs.T.view((seq_len, btch_len, 1))
+        
+                # perform single step of optimization
+                optimizer.zero_grad()
+                h_0 = self.init_hidden(btch_len)
+                y, _ = self.forward(x, h_0)
+                loss = F.mse_loss(y, outputs)
+                loss.backward()
+                optimizer.step()
+        
+                # store training and test losses for monitoring
+                losses.append(loss.item())
+                test_losses.append(self.calculate_loss(test_data))
+                self.train()
+    
+            # advance learning rate scheduler, if given
+            if lr_scheduler is not None:
+                lr_scheduler.step()
+        
+            # monitor progress
+            print(f"On epoch {n+1}, the average trainig loss is {np.mean(losses):.6f} and average test error is {np.mean(test_losses):.6f}.")
+        
+        end_time = time.time()
+        print(f"Total training time {end_time - start_time}")
+
+# %%
+gru_small = GRU(n_hidden=20)
+
+gru_small.train_model(
+    35, 0.001, train_dataloader_small, testing_data_small,
+    lr_scheduler_func=optim.lr_scheduler.MultiStepLR, lr_params=[[5, 10, 15, 25, 30], 0.9]
+)
+
+# add GRU prediction to the constructed data frame
+test_input, test_targets = testing_data_small.inputs, testing_data_small.targets
+ypred = gru_small.forecast(test_input, len(test_targets), scaler=testing_data_small.scaler)
+ypred = ypred.numpy().flatten()
+
+df.loc[:, "GRU, small"] = np.nan
+df.iloc[(len(df) - len(ypred)):, len(df.columns)-1] = ypred
+
+# %%
+gru_medium = GRU(n_hidden=25)
+
+gru_medium.train_model(
+    30, 0.001, train_dataloader_medium, testing_data_medium,
+    lr_scheduler_func=optim.lr_scheduler.MultiStepLR, lr_params=[[15, 20, 25, 30], 0.9]
+)
+
+# add GRU prediction to the constructed data frame
+test_input, test_targets = testing_data_medium.inputs, testing_data_medium.targets
+ypred = gru_medium.forecast(test_input, len(test_targets), scaler=testing_data_medium.scaler)
+ypred = ypred.numpy().flatten()
+
+df.loc[:, "GRU, medium"] = np.nan
+df.iloc[(len(df) - len(ypred)):, len(df.columns)-1] = ypred
+
+# %%
+gru_large = GRU(n_hidden=30)
+
+gru_large.train_model(
+    40, 0.001, train_dataloader_large, testing_data_large,
+    lr_scheduler_func=optim.lr_scheduler.MultiStepLR, lr_params=[[15, 20, 25, 30], 0.9]
+)
+
+# add GRU prediction to the constructed data frame
+test_input, test_targets = testing_data_large.inputs, testing_data_large.targets
+ypred = gru_large.forecast(test_input, len(test_targets), scaler=testing_data_large.scaler)
+ypred = ypred.numpy().flatten()
+
+df.loc[:, "GRU, large"] = np.nan
+df.iloc[(len(df) - len(ypred)):, len(df.columns)-1] = ypred
+
+# %%
+# plot results for GRUs
+fig, ax = plt.subplots(1, 1, figsize=(4,4))
+
+df.iloc[-20:, :].plot(
+    x="Date",
+    y=["Train", "Test", "GRU, small", "GRU, medium", "GRU, large"],
+    ax=ax,
+    grid=True
+)
+
+ax.xaxis.set_major_locator(mdates.DayLocator(interval=7))
+ax.xaxis.set_minor_locator(mdates.DayLocator())
+
+plt.show()
+
+# %%
+# plot results for all models
+fig, ax = plt.subplots(1, 1, figsize=(5,4))
+
+df.iloc[-20:, :].plot(
+    x="Date",
+    y=["Train", "Test", "LSTM, small", "LSTM, medium", "LSTM, large", "GRU, small", "GRU, medium", "GRU, large"],
+    ax=ax,
+    style=["-", "-"] + 3*["--"] + 3*[":"],
+    grid=True
+)
+
+ax.xaxis.set_major_locator(mdates.DayLocator(interval=7))
+ax.xaxis.set_minor_locator(mdates.DayLocator())
+
+plt.show()
